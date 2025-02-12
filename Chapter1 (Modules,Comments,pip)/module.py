7b182527bd9a125e1468120653242856ab5ea789
SabehShaikh <sabehshaikh201@gmail.com>
2025-02-13 01:27:56 +0500

    chapter1 code added

---
+++ Chapter1 (Modules,Comments,pip)/module.py
@@ -0,0 +1,12 @@
+import pyjokes 
+
+# print("Printing Jokes...")
+
+# this prints a random joke
+
+joke = pyjokes.get_joke()
+print(joke)
+
+'''
+So thanks that was my program (this is multiline comment)
+'''
